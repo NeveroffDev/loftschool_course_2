1a2d88c928debd6cb0b0c7fccf3f8ac559db6620
Anton Neverov <aneverov@seligdar.ru>
2019-09-03 14:51:06 +0900

    Add fullscreen menu

---
+++ build/js/burger-menu.js
@@ -0,0 +1,16 @@
+let burgerButton = document.getElementById('burger-menu');
+let fullScreenMenu = document.getElementById('full-screen-menu');
+let fullScreenClose = document.getElementById('full-screen-close');
+
+burgerButton.addEventListener('click', function (e) {
+    fullScreenMenu.style.opacity = '1';
+    fullScreenMenu.style.left = '0';
+    fullScreenMenu.style.right = '0';
+
+});
+
+fullScreenClose.addEventListener('click', function (e) {
+    fullScreenMenu.style.opacity = '0';
+    fullScreenMenu.style.left = '-100%';
+    fullScreenMenu.style.right = '100%';
+});
