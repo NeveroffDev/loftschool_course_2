b0611b76a2b67a7d95f171fffa99d63e3bf25899
Anton Neverov <aneverov@seligdar.ru>
2019-09-05 08:30:32 +0900

    add mentor fix wave#2

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -6,11 +6,12 @@ burgerButton.addEventListener('click', function (e) {
     fullScreenMenu.style.opacity = '1';
     fullScreenMenu.style.left = '0';
     fullScreenMenu.style.right = '0';
-
+    document.body.style.overflow = 'hidden';
 });
 
 fullScreenClose.addEventListener('click', function (e) {
     fullScreenMenu.style.opacity = '0';
     fullScreenMenu.style.left = '-100%';
     fullScreenMenu.style.right = '100%';
+    document.body.style.overflow = 'initial';
 });
