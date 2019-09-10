aa1d4691ae46a1431aca89baf68f2093292ebe2e
Anton Neverov <aneverov@seligdar.ru>
2019-09-10 14:22:50 +0900

    add form sender fix#6 final)

--- build/js/form-sender.js
+++ build/js/form-sender.js
@@ -1,10 +1,11 @@
 const myForm = document.getElementById('form-order');
 const button = document.getElementById('form-send-button');
 
+
 button.addEventListener('click', (e) => {
     e.preventDefault();
     if (validateForm(myForm)) {
-        const formData = new FormData(myForm);
+        const formData = new FormData();
         formData.append('name', myForm.elements.name.value);
         formData.append('phone', myForm.elements.phone.value);
         formData.append('comment', myForm.elements.comment.value);
@@ -12,11 +13,11 @@ button.addEventListener('click', (e) => {
 
         const xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
-        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-        xhr.send(JSON.stringify(formData));
-
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail/fail');
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.send(formData);
         xhr.addEventListener('load', () => {
-            console.log(xhr.response);
+            showModal(xhr.response.message);
         })
     }
 });
@@ -37,4 +38,33 @@ function validateForm(form) {
 
 function validateField(field) {
     return field.checkValidity();
+}
+
+function showModal(message) {
+    const overlay = document.querySelector('.order-overlay');
+    const text = overlay.querySelector('.order-overlay__text');
+    const box = overlay.querySelector('.order-overlay__box');
+    const buttonClose = overlay.querySelector('.order-overlay__close');
+
+    overlay.style.animation = 'fadeIn .4s ease both';
+    box.style.animation = 'bounceScaleIn .4s ease both';
+    text.textContent = message;
+    document.body.style.overflow = 'hidden';
+
+    function hideOverlay(){
+        overlay.style.opacity = '0';
+        document.body.style.overflow = 'initial';
+        overlay.style.animation = 'fadeOut .4s ease both';
+        box.style.animation = 'bounceScaleOut .4s ease both';
+    }
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape'){
+            hideOverlay();
+        }
+    });
+
+    buttonClose.addEventListener('click', (e) => {
+            hideOverlay();
+    });
 }
