5d0052c66cb0527353150feba90ab6af98a8b551
Anton Neverov <aneverov@seligdar.ru>
2019-09-10 13:39:02 +0900

    add form sender fix#5

--- build/js/form-sender.js
+++ build/js/form-sender.js
@@ -3,21 +3,22 @@ const button = document.getElementById('form-send-button');
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    if (validateForm(myForm)) {}
-        let formData = new FormData(myForm);
-        formData.append('name', myForm.elements.name.value);
-        formData.append('phone', myForm.elements.phone.value);
-        formData.append('comment', myForm.elements.comment.value);
-        formData.append('to', 'webpuritycg@gmail.com');
+    if (validateForm(myForm)) {
+    }
+    let formData = new FormData(myForm);
+    formData.append('name', myForm.elements.name.value);
+    formData.append('phone', myForm.elements.phone.value);
+    formData.append('comment', myForm.elements.comment.value);
+    formData.append('to', 'webpuritycg@gmail.com');
 
-        const xhr = new XMLHttpRequest();
-        xhr.responseType = 'json';
-        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-        xhr.send(FormData);
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+    xhr.send(JSON, FormData);
 
-        xhr.addEventListener('load', () => {
-            console.log(xhr.response.status);
-        })
+    xhr.addEventListener('load', () => {
+        console.log(xhr.response);
+    })
 
 });
 
