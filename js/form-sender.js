6a12887875b7d6fdb4151a1335725de075fcbc2b
Anton Neverov <aneverov@seligdar.ru>
2019-09-10 13:44:28 +0900

    add form sender fix#5

--- js/form-sender.js
+++ js/form-sender.js
@@ -4,22 +4,21 @@ const button = document.getElementById('form-send-button');
 button.addEventListener('click', (e) => {
     e.preventDefault();
     if (validateForm(myForm)) {
+        const formData = new FormData(myForm);
+        formData.append('name', myForm.elements.name.value);
+        formData.append('phone', myForm.elements.phone.value);
+        formData.append('comment', myForm.elements.comment.value);
+        formData.append('to', 'webpuritycg@gmail.com');
+
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        xhr.send(JSON.stringify(formData));
+
+        xhr.addEventListener('load', () => {
+            console.log(xhr.response);
+        })
     }
-    let formData = new FormData(myForm);
-    formData.append('name', myForm.elements.name.value);
-    formData.append('phone', myForm.elements.phone.value);
-    formData.append('comment', myForm.elements.comment.value);
-    formData.append('to', 'webpuritycg@gmail.com');
-
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-    xhr.send(JSON, FormData);
-
-    xhr.addEventListener('load', () => {
-        console.log(xhr.response);
-    })
-
 });
 
 function validateForm(form) {
